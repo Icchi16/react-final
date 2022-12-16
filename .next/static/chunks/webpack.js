c38d1f79749f76f453abc881d6556ca33a74b588
Icchi16 <duynghiemhn16@gmail.com>
2022-12-16 13:02:48 +0700

    change to radio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bfebf7404c08f15"; }
+/******/ 		__webpack_require__.h = function() { return "be8a66041b0c6056"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
