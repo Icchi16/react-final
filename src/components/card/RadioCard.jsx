25409c31a62338f61c482091c5f703233a3df6f2
Icchi16 <duynghiemhn16@gmail.com>
2022-12-21 21:07:11 +0700

    why

--- src/components/card/RadioCard.jsx
+++ src/components/card/RadioCard.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Avatar, Skeleton, Switch } from "antd";
 
 const RadioCard = () => {
   const { Meta } = Card;
+  const [loading, setLoading] = useState(true);
+  const onChange = (checked) => {
+    setLoading(!checked);
+  };
   return (
     <div>
-      <Card></Card>
+      <Card hoverable cover={<div style={{ width: 30, height: 50 }}></div>}>
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
     </div>
   );
 };
