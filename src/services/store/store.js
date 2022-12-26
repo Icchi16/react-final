629b45d1cabc3fc074951cba1df29f9f42158df6
Icchi16 <duynghiemhn16@gmail.com>
2022-12-26 19:44:28 +0700

    redux

--- src/services/store/store.js
+++ src/services/store/store.js
@@ -1,6 +1,31 @@
 import { configureStore, createAsyncThunk } from "@reduxjs/toolkit";
 import { randomRadioReducer } from "../features/randomRadio/randomRadio.slice";
 
+const createStore = (preloadedState) => {
+  return configureStore({
+    reducer: {
+      click: randomRadioReducer,
+    },
+    preloadedState,
+  });
+};
+
+let store;
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? createStore(preloadedState);
+  if (preloadedState && store) {
+    _store = createStore({ ...store.getState(), ...preloadedState });
+    store = undefined;
+  }
+
+  if (typeof window === "undefined") {
+    return _store;
+  } else if (!store) {
+    store = _store;
+  }
+  return _store;
+};
+
 export const store = configureStore({
   reducer: {
     click: randomRadioReducer,
