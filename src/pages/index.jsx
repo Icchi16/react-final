07f013d8f1eef388cd2290174ea97ac2b9c6ce5c
Icchi16 <duynghiemhn16@gmail.com>
2022-12-28 14:02:53 +0700

    async doesn't work

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import HomeSwiper from "../components/swiper/homeSwiper/HomeSwiper";
-import { click } from "../services/slices/topClickRadio/topClickRadio.slice";
+import { wrapper } from "../services/store/store";
 
-const App = () => {
+const App = (props) => {
+  const { data } = props;
+  console.log(data);
   return (
     <div className="container mx-auto">
       <div>
         <h2 className="mb-12">For you {process.env.TOP_CLICK}</h2>
         <HomeSwiper />
-        <h1 onClick={() => dispatch(click())}>Click here</h1>
+        <h1>Click here {data}</h1>
       </div>
     </div>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ resolvedUrl }) => {
+      console.log("resolvedUrl", resolvedUrl);
+
+      return {
+        props: {
+          data: resolvedUrl,
+        },
+      };
+    }
+);
 
 export default App;
