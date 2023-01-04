25ff3f88c946b51635c8efc10c25c45d3c93f214
Icchi16 <duynghiemhn16@gmail.com>
2023-01-04 12:53:09 +0700

    change Swiper comp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,12 +12,38 @@ const App = (props) => {
   const topClickRadio = useSelector(selectTopClickRadio);
   const dispatch = useDispatch();
 
+  const forYouSlides = {
+      data: "sds",
+      styling: [{ home: "home" }, { many: "many" }],
+    },
+    hotSlides = [],
+    artistSlides = [];
+
   return (
     <div className="container mx-auto">
       <div>
         <h2 className="mb-7 mt-0">For You</h2>
 
-        <HomeSwiper />
+        <div>
+          <h3>Stations Near You</h3>
+          <p>Live and local, just for you</p>
+          <HomeSwiper slides={forYouSlides} />
+        </div>
+
+        <div>
+          <h3>The Latest On Bills Safety Damar Hamlin's Health</h3>
+          <p>Live and local, just for you</p>
+          <HomeSwiper slides={hotSlides} />
+        </div>
+
+        <div>
+          <h3>Follow Your Leagues & Teams</h3>
+          <p>
+            Get the best audio in your favorite teams instantly in one place
+          </p>
+          <HomeSwiper slides={artistSlides} />
+        </div>
+
         <h1
           onClick={() => {
             dispatch(getTopClickRadio("test"));
